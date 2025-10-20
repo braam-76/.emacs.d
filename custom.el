2e281cb38fbf1427fb1cd4bbc3b930b59d9d9113
braam-76 <braam76.gamidov@gmail.com>
2025-10-20 18:31:56 +0300

    Changed package manager from package.el to straight.el. Works great

--- custom.el
+++ custom.el
@@ -5,13 +5,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("f5f070872db3e4d8b82dbb2f3b1c60beca86fc93327a38ebddd22070458a14bc"
-     "daa27dcbe26a280a9425ee90dc7458d85bd540482b93e9fa94d4f43327128077"
-     default))
- '(package-selected-packages
-   '(auto-dark company consult eglot embark eziam-themes general
-	       kanagawa-themes lsp-ui marginalia orderless sudo-edit
-	       vertico yasnippet)))
+	 '("19978ce130183958b0dcc33bda9642715c50c52f3ce487a3c12e235813687d44"
+		 "daa27dcbe26a280a9425ee90dc7458d85bd540482b93e9fa94d4f43327128077"
+		 default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,58 +1,88 @@
-;;; package --- Summary -*- lexical-binding: t; -*-
-;;; Commentary:
-;; My Emacs config
+package --- Summary -*- lexical-binding: t; -*-
+Commentary:
+;;; My Emacs config
 
 ;;; Code:
 
+(setq gc-cons-threshold (* 50 1000 1000))
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;(setenv "PATH" "/home/braam/.local/share/pnpm:/home/linuxbrew/.linuxbrew/sbin:/home/braam/.local/bin:/home/braam/.local/share/bob/nvim-bin:/home/linuxbrew/.linuxbrew/bin:/home/braam/.config/emacs/bin:/home/braam/.cargo/bin:/home/braam/.sdkman/candidates/maven/current/bin:/home/braam/.sdkman/candidates/java/current/bin:/home/braam/.sdkman/candidates/gradle/current/bin:/home/braam/.local/share/nvm/v22.19.0/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/home/braam/.local/bin")
-
-;;; SLIME installed with quicklisp
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-;; Replace "sbcl" with the path to your implementation
-(setq inferior-lisp-program "sbcl")
-
-;;; set FISH as default shell instead of BASH
-(setq-default explicit-shell-file-name "/usr/bin/fish")
-
-;;; ui stuff
-(setq inhibit-startup-message t)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(setq ring-bell-function 'ignore) ;; turn of that goddamn beep!
-
-(setq scroll-margin 0
-      scroll-conservatively 100000
-      scroll-preserve-screen-position 1)
-
-(global-hl-line-mode t)
-(electric-pair-mode 1)
+(straight-use-package 'use-package)
 
 ;;; fonts
+(set-face-attribute 'font-lock-comment-face nil
+                    :slant 'italic)
+(set-face-attribute 'font-lock-keyword-face nil
+                    :slant 'italic)
+
 (let ((mono-spaced-font "ZedMono Nerd Font")
       (proportionately-spaced-font "Sans"))
   (set-face-attribute 'default nil :family mono-spaced-font :height 120)
   (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 1.0)
   (set-face-attribute 'variable-pitch nil :family proportionately-spaced-font :height 1.0))
 
-(set-face-attribute 'font-lock-comment-face nil
-                    :slant 'italic)
-(set-face-attribute 'font-lock-keyword-face nil
-                    :slant 'italic)
-
 (add-to-list 'default-frame-alist '(font . "ZedMono Nerd Font-12"))
 (setq-default line-spacing 0.12)
 
-;;; backup and custom files
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup")))
+(use-package emacs
+  :custom
+  (menu-bar-mode nil)
+  (tool-bar-mode nil)
+  (scroll-bar-mode nil)
 
-(setq backup-by-copying-when-linked t)
-(setq backup-by-copying             t) ; Backup by copying rather renaming
+  (delete-selection-mode t)
+  (electric-indent-mode nil)
+  (electric-pair-mode t)
 
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(load custom-file :no-error-if-file-is-missing)
+  (ring-bell-function 'ignore)
+
+  (blink-cursor-mode nil)
+  (global-auto-revert-mode t)
+
+  (global-display-line-numbers-mode t)
+
+  (mouse-wheel-progressive-speed nil)
+  (scroll-conservatively 10)
+
+  (tab-width 2)
+
+  (auto-save-default nil)
+  (make-backup-files nil)
+
+  :hook (prog-mode . (lambda () (hs-minor-mode t)))
+  :config
+  (setq custom-file (locate-user-emacs-file "custom.el"))
+  (load custom-file :no-error-if-file-is-missing)
+
+	(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+	(load custom-file 'noerror)
+
+  (setq backup-directory-alist '(("." . "~/.emacs.d/backup")))
+  (setq backup-by-copying-when-linked t)
+  (setq backup-by-copying             t) ; Backup by copying rather renaming
+  
+  :bind (([escape] . keyboard-escape-quit) ;; Makes Escape quit prompts (Minibuffer Escape)
+         ;; Zooming In/Out
+         ("C-+" . text-scale-increase)
+         ("C--" . text-scale-decrease)
+         ("<C-wheel-up>" . text-scale-increase)
+         ("<C-wheel-down>" . text-scale-decrease))
+  )
 
 ;;; turn off warnings when installing packages
 (add-to-list
@@ -61,145 +91,207 @@
    (display-buffer-no-window)
    (allow-no-window . t)))
 
-;;; PACKAGES
-;;; use-package
-(require 'package)
-(package-initialize)
-(setq package-archives
-      '(("gnu"      . "http://elpa.gnu.org/packages/")
-        ("nongnu"   . "http://elpa.nongnu.org/nongnu/")
-        ("melpa"    . "http://melpa.org/packages/")))
-(setq package-enable-at-startup nil
-      package-check-signature   nil)
-(package-initialize)
+(use-package general
+	:straight t
+  :config
+  (general-emacs-define-key 'global :prefix "C-c"
+   "." '(find-file :wk "Find file")
+   "TAB" '(comment-line :wk "Comment lines")
+   "t" '(eat :wk "Eat terminal")
+	 ))
 
-;; Refresh package archives if needed
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-;; Now load use-package
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)  ;; Automatically ensure packages
+(use-package multiple-cursors
+	:straight t
+	:config 
+	(general-emacs-define-key 'global
+		"C-." (mc/mark-next-like-this :wk "Mark next like this")
+		"C-," (mc/mark-previous-like-this :wk "Mark previous like this")
+		"C-'" (mc/mark-all-like-this :wk "Mark all like this")
+		)
+	:init
+	(define-key mc/keymap (kbd "<return>") nil) ;; so i can insert newline while using multiple cursors
+	)
 
 ;;; themes
-(use-package kanagawa-themes :ensure t)
-(use-package eziam-themes :ensure t)
+(use-package kanagawa-themes :straight t)
+(use-package eziam-themes :straight t)
+(use-package goose-theme :straight t)
+(use-package badger-theme :straight t)
+
 
 (use-package auto-dark
-  :ensure t
+	:straight t
   :custom
-  (auto-dark-themes '((kanagawa-wave) (eziam-light)))
+  (auto-dark-themes '((badger) (goose)))
   (auto-dark-polling-interval-seconds 5)
   :init (auto-dark-mode))
 
+;;; status line
+(use-package doom-modeline
+	:straight t
+  :custom
+  (doom-modeline-height 25) ;; Set modeline height
+  :hook (after-init . doom-modeline-mode))
+
+;;; nerd-icons
+(use-package nerd-icons
+	:straight t
+  :if (display-graphic-p))
+
+;;; Use $PATH from shell to find lsp's and any other programs
+(use-package exec-path-from-shell
+	:straight t
+  :init
+  (exec-path-from-shell-initialize))
+
+;;; eglot
+(use-package eglot
+  :straight nil
+  :hook (c-mode . eglot-ensure)
+	(c++-mode . eglot-ensure)
+	(java-mode . eglot-ensure)
+	(js-ts-mode . eglot-ensure)
+	(tsx-ts-mode . eglot-ensure)
+  (eglot-events-buffer-size 0)
+  (eglot-autoshutdown t)
+  (eglot-report-progress nil))
+
+;;; treesitter (automatically installs treesitter grammars)
+(use-package treesit-auto
+	:straight t
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
+;;; yasnippet
+(use-package yasnippet-snippets :straight t :hook (prog-mode . yas-minor-mode))
+
 ;;; which-key
-(use-package which-key :ensure t :init (which-key-mode 1))
+(use-package which-key :straight t :hook (prog-mode . which-key-mode))
+
+;;; eat
+(use-package eat
+	:straight t
+  :hook ('eshell-load-hook #'eat-eshell-mode))
+
+;;; magit
+(use-package magit
+	:straight t
+  :defer
+  :custom (magit-diff-refine-hunk (quote all)) ;; Shows inline diff
+  :config (define-key transient-map (kbd "<escape>") 'transient-quit-one) ;; Make escape quit magit prompts
+  )
+
+;;; corfu
+(use-package corfu
+	:straight t
+  ;; Optional customizations
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                 ;; Enable auto completion
+  (corfu-auto-prefix 2)          ;; Minimum length of prefix for auto completion.
+  (corfu-popupinfo-mode t)       ;; Enable popup information
+  (corfu-popupinfo-delay 0.5)    ;; Lower popup info delay to 0.5 seconds from 2 seconds
+  (corfu-separator ?\s)          ;; Orderless field separator, Use M-SPC to enter separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+  (completion-ignore-case t)
+
+  ;; Emacs 30 and newer: Disable Ispell completion function.
+  ;; Try `cape-dict' as an alternative.
+  (text-mode-ispell-word-completion nil)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (tab-always-indent 'complete)
+
+  (corfu-preview-current nil) ;; Don't insert completion without confirmation
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :init
+  (global-corfu-mode))
+(use-package nerd-icons-corfu
+	:straight t
+  :after corfu
+  :init (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+
+(use-package cape
+	:straight t
+  :after corfu
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+
+  ;; The functions that are added later will be the first in the list
+  (add-hook 'completion-at-point-functions #'cape-dabbrev) ;; Complete word from current buffers
+  (add-hook 'completion-at-point-functions #'cape-dict) ;; Dictionary completion
+  (add-hook 'completion-at-point-functions #'cape-file) ;; Path completion
+  (add-hook 'completion-at-point-functions #'cape-elisp-block) ;; Complete elisp in Org or Markdown mode
+  (add-hook 'completion-at-point-functions #'cape-keyword) ;; Keyword completion
+
+  ;;(add-hook 'completion-at-point-functions #'cape-abbrev) ;; Complete abbreviation
+  ;;(add-hook 'completion-at-point-functions #'cape-history) ;; Complete from Eshell, Comint or minibuffer history
+  ;;(add-hook 'completion-at-point-functions #'cape-line) ;; Complete entire line from current buffer
+  ;;(add-hook 'completion-at-point-functions #'cape-elisp-symbol) ;; Complete Elisp symbol
+  ;;(add-hook 'completion-at-point-functions #'cape-tex) ;; Complete Unicode char from TeX command, e.g. \hbar
+  ;;(add-hook 'completion-at-point-functions #'cape-sgml) ;; Complete Unicode char from SGML entity, e.g., &alpha
+  ;;(add-hook 'completion-at-point-functions #'cape-rfc1345) ;; Complete Unicode char using RFC 1345 mnemonics
+  )
 
 ;;; orderless
 (use-package orderless
-  :ensure t
-  :custom
+	:straight t 
+  :config
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles partial-completion))))
   (completion-category-defaults nil)
   (completion-pcm-leading-wildcard t))
 
-;;; yasnippet
-(use-package yasnippet :ensure t :init (yas-global-mode 1))
-
-;;; sudo-edit
-(use-package sudo-edit :ensure t)
-
 ;;; vertico
 (use-package vertico
-  :ensure t
+	:straight t
   :custom (vertico-cycle t)
   :init (vertico-mode 1))
 
 ;;; marginalia
-(use-package marginalia :ensure t :init (marginalia-mode 1))
+(use-package marginalia :straight t :init (marginalia-mode 1))
 
 ;;; consult
 (use-package consult
-  :ensure t
+	:straight t
   :defer t
   :init (advice-add #'register-preview :override #'consult-register-window)
   (setq xref-show-xrefs-function #'consult-xref
         xref-show-definitions-function #'consult-xref))
 
+;;; sudo-edit
+(use-package sudo-edit :straight t)
+
 ;;; embark
-(use-package embark
-  :ensure t
-  :defer t)
+(use-package embark :straight t :defer t)
 
 ;;; embark-consult
 (use-package embark-consult
-  :ensure t
   :straight t
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
-;;; treesitter-auto
-(use-package treesit-auto
-  :ensure t
-  :straight t
-  :after emacs
-  :custom
-  (treesit-auto-install 'prompt)
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode t))
+(use-package sly :straight t)
 
-;;; corfu
-(use-package corfu
-  :ensure t
-  :straight t
-  :defer t
-  :custom
-  (corfu-auto nil)                        ;; Only completes when hitting TAB
-  ;; (corfu-auto-delay 0)                ;; Delay before popup (enable if corfu-auto is t)
-  (corfu-auto-prefix 1)                  ;; Trigger completion after typing 1 character
-  (corfu-quit-no-match t)                ;; Quit popup if no match
-  (corfu-scroll-margin 5)                ;; Margin when scrolling completions
-  (corfu-max-width 50)                   ;; Maximum width of completion popup
-  (corfu-min-width 50)                   ;; Minimum width of completion popup
-  (corfu-popupinfo-delay 0.5)            ;; Delay before showing documentation popup
-  :config
-  (if ek-use-nerd-fonts
-      (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
-  :init
-  (global-corfu-mode)
-  (corfu-popupinfo-mode t))
-
-;;; Flymake
-(use-package flymake
-  :ensure nil          ;; This is built-in, no need to fetch it.
-  :defer t
-  :init (flymake-mode 1)
-  :custom
-  (flymake-margin-indicators-string
-   '((error "!»" compilation-error) (warning "»" compilation-warning)
-     (note "»" compilation-info))))
-
-(use-package company
-  :ensure t
-  :init (global-company-mode 1))
-
-;;; eglot
-(package-install 'eglot)
-(require 'eglot)
-(define-key eglot-mode-map (kbd "C-c e f n")  flymake-goto-next-error)
-(define-key eglot-mode-map (kbd "C-c e f p")  flymake-goto-prev-error)
-(define-key eglot-mode-map (kbd "C-c e r")    eglot-rename)
-
-;;; PATH
-(use-package exec-path-from-shell
-  :ensure t)
-(when (daemonp)
-  (exec-path-from-shell-initialize))
+(use-package geiser-chez :straight t)
+(use-package geiser 
+	:straight t
+	:init
+	(setq geizer-chez-binary "/usr/bin/scheme"))
 
 (provide 'init)
 ;;; init.el ends here
